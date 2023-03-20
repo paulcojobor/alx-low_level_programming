c4f03951cde2d19fdf39bfa5b2802065686242d5
paulcojobor <paulcojobor@gmail.com>
2023-03-19 23:15:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	write("\tand that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	write("\tand that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
