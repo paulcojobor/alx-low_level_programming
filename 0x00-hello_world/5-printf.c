a5c4e9ffed20683e190836b5c7189fc43d19a41a
paulcojobor <paulcojobor@gmail.com>
2023-03-19 21:01:59 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("\t with proper grammar, but the outcome is a piece of art, \n");
+	printf("\twith proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
