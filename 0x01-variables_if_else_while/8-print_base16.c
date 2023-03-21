7ee50491fde4a7cca591909f912befc4dbdbf7c9
paulcojobor <paulcojobor@gmail.com>
2023-03-20 21:59:44 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,20 @@
  */
 int main(void)
 {
-        int i;
+	char c;
 
-        for (i=48;i<58;i++)
-        {
-		putchar(i);
-        }
-	for (i=97;i<103;i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return(0);
-} 
+        int d;
+	c = 'a';
+	while (d < 10)
+	{
+		putchar(d + '0');
+		d++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
