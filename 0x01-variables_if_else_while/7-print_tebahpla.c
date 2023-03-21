52942498606109a42bc217cf6c29780bca481545
paulcojobor <paulcojobor@gmail.com>
2023-03-20 21:47:30 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,13 +5,14 @@
  * Return: Always 0(Success)
  */
 int main(void)
-{       
-        char ch;
+{
+	char c;
 
-        for (ch='z',ch >= 'a'; ch-)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return(0);
+	c = 'z';
+	for (c = 'z'; c >= 'a'; c--;)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
