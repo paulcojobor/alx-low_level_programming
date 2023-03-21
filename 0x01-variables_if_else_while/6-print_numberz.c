7c00c9567ef0baecab5e4ad8ddf408ed7a6bb85a
paulcojobor <paulcojobor@gmail.com>
2023-03-20 19:54:48 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,13 @@
  * Return: Always 0(Success)
  */
 int main(void)
-{       
-        int i;
+{
+	int i;
 
-        for (i=48;i<58;i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return(0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
